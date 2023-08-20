e7a703b30054edc39899f5fe7176117715db2946
sujin-park0607 <goweg0607@gmail.com>
2023-08-20 18:38:13 +0900

    feat: add cors - CORS의 preflight허용을 위해 security 수정

--- src/main/java/com/newyearletter/newyearletter/configuration/CorsConfig.java
+++ src/main/java/com/newyearletter/newyearletter/configuration/CorsConfig.java
@@ -10,10 +10,10 @@ public class CorsConfig  implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**") //해당 서버의 모든 URL 요청 허용
-                .allowedOrigins("https://www.newyearletter.com", "localhost:8484") //원하는 도메인만 허용
-                .allowedMethods("GET", "POST", "PATCH", "PUT", "DELETE") //원하는 HTTP 메서드 허용
-                .allowedHeaders("headers") // 헤더 네임
-                .maxAge(3000); //preflight요청 캐싱
+                .allowedOrigins("https://www.newyearletter.com", "*", "localhost:8484") //원하는 도메인만 허용
+                .allowedMethods("*") //원하는 HTTP 메서드 허용
+                .allowedHeaders("*"); // 헤더 네임
+//                .maxAge(3000); //preflight요청 캐싱
     }
 
 }
