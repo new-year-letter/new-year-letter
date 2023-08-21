1186bf35c2ae4b723882910932affcd4d406a7e6
Heebeom Yang <takeny1998@gmail.com>
2023-08-21 17:51:34 +0900

    Credentials 헤더 추가

--- src/main/java/com/newyearletter/newyearletter/configuration/WebConfig.java
+++ src/main/java/com/newyearletter/newyearletter/configuration/WebConfig.java
@@ -11,7 +11,8 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**") //해당 서버의 모든 URL 요청 허용
                 .allowedOrigins("https://www.newyearletter.com", "localhost:8484") //원하는 도메인만 허용
-                .allowedMethods("*");
+                .allowedMethods("*")
+                .allowCredentials(true);
     }
 
 }
